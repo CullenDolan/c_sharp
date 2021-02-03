75e5f58492893e21d323a45d5aaf164231b91ec8
Cullen D <cullen.dolan@gmail.com>
2021-02-03 08:57:33 -0600

    created a mini calculator

--- Program.cs
+++ Program.cs
@@ -6,20 +6,11 @@ namespace test_c_sharp
     {
         static void Main(string[] args)
         {
-            int age = 30;
-            string myName = "Cullen" ;
-            char grade = 'A'; // can only be one character
-            float float_val = 3.1415F; // least precise
-            double doub_val = 3.1415;
-            decimal dec_val = 3.1415M; // most precise
-            bool isMale = true;
-            Console.WriteLine("My name is " + myName + " I am " + age + " years old.");
-            Console.WriteLine(myName.Length); //examples of attribute
-            Console.WriteLine(myName.ToUpper()); // method
-            Console.WriteLine(myName[5]); // string index
-            Console.WriteLine(myName.IndexOf("u")); // find where something starts
-            Console.WriteLine(Math.Floor(-4.55)); //another method that has math operations
-
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            Double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
 
         }
     }
