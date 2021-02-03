6378e2b845c4f9f651834b5081be9dd8f12b97d5
Cullen D <cullen.dolan@gmail.com>
2021-02-02 22:29:26 -0600

    testing data types

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace test_c_sharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int age = 30;
+            string myName = "Cullen" ;
+            char grade = 'A'; // can only be one character
+            float float_val = 3.1415F; // least precise
+            double doub_val = 3.1415;
+            decimal dec_val = 3.1415M; // most precise
+            bool isMale = true;
+            Console.WriteLine("My name is " + myName + " I am " + age + " years old.");
+            Console.WriteLine(grade);
+            Console.WriteLine(float_val);
+            Console.WriteLine(doub_val);
+            Console.WriteLine(dec_val);
+            Console.WriteLine(isMale);
+
         }
     }
 }
