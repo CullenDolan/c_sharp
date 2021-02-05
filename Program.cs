cbe72da341106d78727e94773972f726ed58d92b
Cullen D <cullen.dolan@gmail.com>
2021-02-04 21:52:22 -0600

    if statements

--- Program.cs
+++ Program.cs
@@ -6,30 +6,15 @@ namespace test_c_sharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetDay(5));
-
-        }
-        // SWITCH STATEMENTS
-        static string GetDay(int dayNum)
-        {
-            string dayName;
-
-            switch(dayNum)
+            int index = 1;
+            while (index < 5)
             {
-                case 0:
-                    dayName = "Sunday";
-                    break;
-                case 1:
-                    dayName = "Monday";
-                    break;
-                case 2:
-                    dayName = "Tuesday";
-                    break;
-                default:
-                    dayName = "Invalid day number";
-                    break;
+                Console.WriteLine(index);
+                index++;
             }
-            return dayName; 
+            
+
         }
+        
     }
 }
